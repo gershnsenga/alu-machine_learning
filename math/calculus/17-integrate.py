49b4bdf956cb3ac0021a4e5f38c5c67e50d92afb
Gershom Nsengiyumva <g.nsengiyum@alustudent.com>
2025-06-14 20:37:26 +0200

    add the missing parts

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python3
-""" defines a function that calculates the integral of a polynomial """
+"""Derivative of a polynomial"""
 
 
 def poly_integral(poly, C=0):
-    """
-    calculates the integral of the given polynomial
-
-    Parameters:
-        poly (list): list of coefficients representing a polynomial
-            the index of the list represents the power of x
-            the coefficient belongs to
-        C (int): the integration constant
-
-    Returns:
-        a new list of coefficients representing the derivative
-            the returned list is as small as possible
-            if a coefficient is a whole number, it is represented by an int
-        None, if poly or C are not valid
-    """
-    if type(poly) is not list or len(poly) < 1:
+    """Calculates the integral of a polynomial"""
+    integral = []
+    # check for the validity of the inputs
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
-    if type(C) is not int and type(C) is not float:
-        return None
-    for coefficient in poly:
-        if type(coefficient) is not int and type(coefficient) is not float:
-            return None
-    if type(C) is float and C.is_integer():
-        C = int(C)
-    integral = [C]
-    for power, coefficient in enumerate(poly):
-        if (coefficient % (power + 1)) is 0:
-            new_coefficient = coefficient // (power + 1)
-        else:
-            new_coefficient = coefficient / (power + 1)
-        integral.append(new_coefficient)
-    while integral[-1] is 0 and len(integral) > 1:
-        integral = integral[:-1]
-    return integral
+    # calculate the cofficients
+    for i in range(len(poly)-1, 0, -1):
+        integral.append(poly[i]/(i+1))
+
+    integral.append(poly[0])
+    integral.append(C)
+    # fit the output like on the intranet
+    if len(poly) == 1 and poly[0] == 0:
+        integral = [C]
+    for i in range(len(integral)):
+        if integral[i] % 1 == 0:
+            integral[i] = int(integral[i])
+
+    return integral[::-1]
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-""" defines a function that calculates a summation """
+"""Calculate sum of squares"""
 
 
 def summation_i_squared(n):
-    """
-    calculates summation of i^2 from i=1 to n
-
-    utilizes Faulhaber's formula for power of 2:
-        sum of i^2 from i=1 to n = (n * (n + 1) * (2n + 1)) / 6
-                                   or ((n^3) / 3) + ((n^2) / 2) + (n / 6)
-    """
-    if type(n) is not int or n < 1:
+    """Calculate sum of squares up to n"""
+    if not isinstance(n, (int, float)) or n != int(n) or n < 1:
         return None
-    sigma_sum = (n * (n + 1) * ((2 * n) + 1)) / 6
-    return int(sigma_sum)
+    return sum(map(lambda x: x*x, range(1, n+1)))
